74c1d3a100834db547b5fc72ef9b5e824fbfd418
MikeWazowski666 <mikkoliver+github@gmail.com>
2022-08-29 01:25:55 +0300

    first commit

---
+++ completed/2.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"time"
+)
+
+type Client struct {
+	name   string
+	incMsg chan string
+	outMsg chan string
+	conn   net.Conn
+	reader *bufio.Reader
+	writer *bufio.ReadWriter
+}
+
+type ChatRoom struct {
+	time   time.Time
+	client *Client
+	text   string
+}
+
+func writeToChannel(c chan int, x int) {
+	fmt.Println(x)
+	c <- x
+	close(c)
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: 1.go [PORT] [TCP/UDP]\nA TCP/UDP commandline chat service.\n")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 2 {
+		fmt.Println("Some arguments are missing.")
+		os.Exit(1)
+	}
+	ch := make(chan int)
+	writeToChannel(ch, 12)
+	time.Sleep(1 * time.Second)
+	writeToChannel(ch, 34)
+
+	// port := args[0]
+	// proto := strings.ToLower(args[1])
+	//
+	// l, err := net.Listen(proto, ":"+port)
+	// if err != nil {
+	// 	os.Exit(1)
+	// }
+	// defer l.Close()
+
+	// for {
+	// 	conn, err := l.Accept()
+	// 	if err != nil {
+	// 		os.Exit(1)
+	// 	}
+
+	// 	go func(c net.Conn) {
+	// 		buffer := make([]byte, 1024)
+	// 		_, err := c.Read(buffer)
+	// 		if err != nil {
+	// 			panic(err)
+	// 		}
+
+	// 		fmt.Print(string(buffer))
+
+	// 		conn.Close()
+	// 	}(conn)
+	// }
+}
+
+func handleRequest(conn net.Conn) {
+	buf, read_err := ioutil.ReadAll(conn)
+	if read_err != nil {
+		fmt.Println("failed:", read_err)
+		return
+	}
+	fmt.Println("Got: ", string(buf))
+
+	_, write_err := conn.Write([]byte("Message received.\n"))
+	if write_err != nil {
+		fmt.Println("failed:", write_err)
+		return
+	}
+	conn.Close()
+}
---
+++ completed/proxy.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+	//"strings"
+	"bytes"
+)
+
+func main() {
+	fmt.Println(os.Args)
+	if len(os.Args) != 4 {
+		fmt.Println("prog listen_port remote_host, remote_port")
+		os.Exit(3)
+    }
+	listen_port := os.Args[1]
+	rmt_host := os.Args[2]
+	rmt_port := os.Args[3]
+	fmt.Println("localhost:" +  listen_port + "  -> " + rmt_host+":"+rmt_port)
+	ln, err := net.Listen("tcp", ":"+listen_port)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			panic(err)
+		}
+
+		go portFWD(conn, rmt_host, rmt_port)
+	}
+}
+
+func portFWD(conn net.Conn, rmt_host string, rmt_port string) {
+
+	proxy, err := net.Dial("tcp", rmt_host+":"+rmt_port)
+	if err != nil {
+		panic(err)
+	}
+
+	go func(){
+		defer conn.Close()
+		defer proxy.Close()
+		
+		//experiment part
+ 		var buf bytes.Buffer
+		len, err := buf.ReadFrom(proxy)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Printf("Message size = %d\n", len)
+		}
+   		fmt.Println("total size:", buf.Len())
+		_, err = conn.Write(buf.Bytes())
+
+		//io.Copy(conn,proxy) //working part
+	}()
+	
+	go func(){
+		defer conn.Close()
+		defer proxy.Close()
+		io.Copy(proxy, conn)
+	}()
